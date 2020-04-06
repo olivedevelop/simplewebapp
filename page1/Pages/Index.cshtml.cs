c395f961eed2ecb826d5e9d0b26bfc2ca91e143f
olivedevelop <otuch@hawk.iit.edu>
2020-04-06 14:48:45 -0500

    yes10

--- page1/Pages/Index.cshtml.cs
+++ page1/Pages/Index.cshtml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace page1.Pages
 {
--- page1/Pages/Privacy.cshtml.cs
+++ page1/Pages/Privacy.cshtml.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-
-namespace page1.Pages
+﻿namespace page1.Pages
 {
     public class PrivacyModel : PageModel
     {
